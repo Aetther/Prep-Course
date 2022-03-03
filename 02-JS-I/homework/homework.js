4861548a70b65728a3d240719f532d44bdcc71ae
¨Aether¨ <Aether@live.com>
2022-03-03 17:30:16 -0300

    Sigo tratando de arreglar el problema para ejecutar el test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -111,7 +111,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ((num % 2) = 0) {
+  if ( (num % 2) = 0) {
     return true;
   } else {
     return false;
@@ -122,7 +122,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ((num % 2 != 0)){
+  if (( num % 2 != 0)){
     return true;
   } else {
     return false;
