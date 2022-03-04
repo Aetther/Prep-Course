9ed76f8755f6627413df780eeeb361eaa1a6599d
¨Aether¨ <Aether@live.com>
2022-03-04 19:30:41 -0300

    Ejercicios resueltos JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -18,7 +18,7 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-  if (edad => 18) {
+  if (edad >= 18) {
     return "Allowed"
   } else {
     return "Not allowed"
@@ -131,17 +131,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3 && num1 > 0) {
-    return "Número 1 es mayor y positivo";
-  }
-  else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
-  }
-  else if (num3 > num1 && num3 > num1) {
-    return num3+1;
   }
   else if (num1 === 0 || num2 === 0 || num3 === 0) {
     return "Error";
+  }
+  else if (num1 > num2 && num1 > num3 && num1 > 0){
+    return "Número 1 es mayor y positivo"
+  }
+  else if (num3 < num1 && num3 < num2) {
+    return num3 + 1;
   }
   else {
     return false;
@@ -175,7 +175,7 @@ function esVerdadero(valor){
   if (valor === true) {
     return "Soy verdadero";
   } else {
-    return false;
+    return "Soy falso";
   }
 }
 
@@ -193,7 +193,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.length > 99 && numero.length < 1000) {
+  if (numero > 99 && numero < 1000) {
     return true;
   }
   else {
